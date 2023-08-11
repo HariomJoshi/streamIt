e13a31595693d482007ea212c81be9b752ebab0d
Hariom Joshi <goodhariom@gmail.com>
2023-08-11 23:38:26 +0530

    first commit

---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,40 @@
+import "./LoginPage.css"
+import { useState } from "react";
+
+function LoginPage(){
+    let [passType, setPassType] = useState("password");
+    return(
+        <div className ="everyThing">
+            <div className="enterDetails">
+                <div className="centerPart">
+                    <h1 className="login">Hey, hello! 👋</h1>
+                    <p className="lightText">Enter the information you entered while registering</p>
+                    <legend className = "login">Email: </legend>
+                    <input  className="inp"/>
+                    <legend className = "login">Password: </legend>
+                    <input className = "inp" type = {passType}/>
+                    <div className="showPassword">
+                        <p className = "lightText">Show password: </p>
+                        <input className="checkbox" type="checkbox" onClick={() =>{passType == "password"? setPassType("text"): setPassType("password")}} />    
+                    </div>
+                    <button className="loginBtn">Login</button>
+                    {/* <hr/> */}
+                </div>
+            
+            </div>
+
+
+            <div className="sideText">
+                
+                <div className="centerSquare">
+                    <h1 id="message"><div className="text">Streaming<br/> just got<br/> a whole lot<br/></div>  easier </h1>
+                    <p className="subHeading">Streaming to any platform<br/> from anywhere</p>
+                </div>
+                
+            </div>
+        </div>
+
+    );
+}
+
+export default LoginPage;
